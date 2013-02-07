1748dc7ac08e5d979d1f2775a536e31e5619aba2
cavila <cristobal.avila@avventa.co.cr>
2013-02-07 14:02:21 -0600

    first activity fixes

--- gen/com/cristobal/rockbands/R.java
+++ gen/com/cristobal/rockbands/R.java
@@ -14,15 +14,17 @@ public final class R {
         public static final int bottom_padding=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int a_perfect_circle=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int button1=0x7f080002;
-        public static final int button2=0x7f080006;
-        public static final int button3=0x7f080005;
-        public static final int home=0x7f080003;
-        public static final int menu_settings=0x7f080007;
-        public static final int textView1=0x7f080004;
+        public static final int button2=0x7f080007;
+        public static final int button3=0x7f080006;
+        public static final int home=0x7f080004;
+        public static final int mainHeader=0x7f080005;
+        public static final int menu_settings=0x7f080008;
+        public static final int textView1=0x7f080003;
         public static final int textView2=0x7f080000;
         public static final int textView3=0x7f080001;
     }
