5b995f3bb66ae24e25ba8b28ae22cc7b16430c31
cavila <cristobal.avila@avventa.co.cr>
2013-02-07 09:17:19 -0600

    cleaning project

--- src/com/cristobal/rockbands/Band_1.java
+++ src/com/cristobal/rockbands/Band_1.java
@@ -2,14 +2,13 @@ package com.cristobal.rockbands;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class Band_1 extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_band_1);
 	}
 
 }
