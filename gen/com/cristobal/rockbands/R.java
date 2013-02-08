d0af22e663673e4148cf1919a0d8a79d28a0b109
cavila <cristobal.avila@avventa.co.cr>
2013-02-08 10:25:26 -0600

    button styles added

--- gen/com/cristobal/rockbands/R.java
+++ gen/com/cristobal/rockbands/R.java
@@ -10,33 +10,40 @@ package com.cristobal.rockbands;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int bg_color=0x7f060000;
+        public static final int txt_color=0x7f060001;
+        public static final int txt_info_color=0x7f060002;
+    }
     public static final class dimen {
         public static final int bottom_padding=0x7f040000;
+        public static final int title_size=0x7f040001;
     }
     public static final class drawable {
         public static final int a_perfect_circle=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int nine_inch_nails=0x7f020002;
-        public static final int tool=0x7f020003;
+        public static final int custom_button=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int nine_inch_nails=0x7f020003;
+        public static final int tool=0x7f020004;
     }
     public static final class id {
-        public static final int button1=0x7f080007;
-        public static final int button2=0x7f080006;
-        public static final int button3=0x7f080005;
-        public static final int home=0x7f080000;
-        public static final int imageView1=0x7f080003;
-        public static final int mainHeader=0x7f080004;
-        public static final int menu_settings=0x7f080008;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080002;
+        public static final int bandImage1=0x7f090001;
+        public static final int bandInfo1=0x7f090003;
+        public static final int bandName1=0x7f090002;
+        public static final int button1=0x7f090007;
+        public static final int button2=0x7f090006;
+        public static final int button3=0x7f090005;
+        public static final int home=0x7f090000;
+        public static final int mainHeader=0x7f090004;
+        public static final int menu_settings=0x7f090008;
     }
     public static final class layout {
         public static final int activity_band_1=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_band_1=0x7f070000;
-        public static final int activity_main=0x7f070001;
+        public static final int activity_band_1=0x7f080000;
+        public static final int activity_main=0x7f080001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
@@ -72,10 +79,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
